c2542f09516e0f3ecc3ce06f3deff0bd724d2afa
Sergey Klimov <myxxze@gmail.com>
2022-08-16 21:44:38 +0300

    added func for creating application file and adding api

--- app.py
+++ app.py
@@ -5,13 +5,21 @@ import accinfo
 def check_api():
     try:
         config = configparser.ConfigParser()
-        config.read()
+        config.read('config.ini')
     except:
         pass
     else:
         pass
     finally:
         pass
+
+
+def create_app_config():
+    api_key = input('Enter your API key: ')
+    with open('config.ini', 'w') as file:
+        config = configparser.ConfigParser()
+        config['SETTINGS'] = {'api': api_key}
+        config.write(file)
 
 
 def create_account():
